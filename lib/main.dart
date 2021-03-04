3b537e472e91c4c82a56b903c926b3d9d016c572
Caique Souza Dos Santos <c.souza.dos.santos@avanade.com>
2021-03-03 22:21:44 -0300

    adicionando projeto completo, com bug na visualização dos valores no input

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,22 @@ const request =
 void main() async {
   runApp(MaterialApp(
     home: Home(),
+    theme: ThemeData(
+        hintColor: Colors.amber,
+        primaryColor: Colors.white,
+        inputDecorationTheme: InputDecorationTheme(
+          enabledBorder:
+              OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+          focusedBorder:
+              OutlineInputBorder(borderSide: BorderSide(color: Colors.amber)),
+          hintStyle: TextStyle(color: Colors.amber),
+        )),
   ));
 }
 
 Future<Map> getData() async {
   http.Response response = await http.get(request);
-  return json.decode(response.body)["results"]["currencies"];
+  return json.decode(response.body);
 }
 
 class Home extends StatefulWidget {
@@ -23,9 +33,33 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final realController = TextEditingController();
+  final dolarController = TextEditingController();
+  final euroController = TextEditingController();
 
   double dolar;
   double euro;
+
+  void _realChanged(String text){
+    print(text);
+    double real = double.parse(text);
+    dolarController.text = (real/dolar).toStringAsFixed(2);
+    euroController.text = (real/euro).toStringAsFixed(2);
+  }
+
+  void _dolarChanged(String text){
+    print(text);
+    double dolar = double.parse(text);
+    realController.text = (dolar * this.dolar).toStringAsFixed(2);
+    euroController.text = (dolar * this.dolar/euro).toStringAsFixed(2);
+  }
+
+  void _euroChanged(String text){
+    print(text);
+    double euro = double.parse(text);
+    realController.text = (euro * this.euro).toStringAsFixed(2);
+    dolarController.text = (euro * this.euro / dolar).toStringAsFixed(2);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -58,26 +92,22 @@ class _HomeState extends State<Home> {
                       textAlign: TextAlign.center,
                     ),
                   );
-                }else{
+                } else {
                   dolar = snapshot.data["results"]["currencies"]["USD"]["buy"];
                   euro = snapshot.data["results"]["currencies"]["EUR"]["buy"];
 
                   return SingleChildScrollView(
+                    padding: EdgeInsets.all(10.0),
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.stretch,
                       children: <Widget>[
-                        Icon(Icons.monetization_on, size: 150.0, color: Colors.amber),
-                        TextField(
-                          decoration: InputDecoration(
-                            labelText: "Reais",
-                            labelStyle: TextStyle(color: Colors.amber),
-                            border: OutlineInputBorder(),
-                            prefixText: "R\$"
-                          ),
-                          style: TextStyle(
-                            color: Colors.amber, fontSize: 25.0
-                          ),
-                        )
+                        Icon(Icons.monetization_on,
+                            size: 150.0, color: Colors.amber),
+                        buildTextField("Reais", "R\$", realController, _realChanged),
+                        Divider(),
+                        buildTextField("Dolar", "US", dolarController, _dolarChanged),
+                        Divider(),
+                        buildTextField("Euros", "€", euroController, _euroChanged),
                       ],
                     ),
                   );
@@ -86,4 +116,18 @@ class _HomeState extends State<Home> {
           }),
     );
   }
+}
+
+Widget buildTextField(
+    String label, String prefix, TextEditingController control, Function changedFunction) {
+  return TextField(
+    decoration: InputDecoration(
+        labelText: label,
+        labelStyle: TextStyle(color: Colors.amber),
+        border: OutlineInputBorder(),
+        prefixText: prefix),
+    style: TextStyle(color: Colors.amber, fontSize: 25.0),
+    onChanged: changedFunction,
+    keyboardType: TextInputType.number,
+  );
 }
